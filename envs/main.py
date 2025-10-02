d67fd72e6dd58f963998f9dd15a368ace9320bdf
AJ-SM <anujsidam1108@gmail.com>
2025-10-02 08:06:10 +0530

    init

---
+++ envs/main.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+
+
+
+
+class RocketLander(gym.env):
+    def __init__(self):
+        super.__init__()
+    
+    def reset():
+        pass
+    
+    def step(step,action):
+        pass
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
